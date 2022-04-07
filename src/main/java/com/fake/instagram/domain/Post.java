0f9a322ba9035a975a8066a6cca0ba23f17fedc1
c.cozma <cozmacosmin1992@gmail.com>
2022-04-07 18:13:02 +0200

    post entity

---
+++ src/main/java/com/fake/instagram/domain/Post.java
@@ -0,0 +1,35 @@
+package com.fake.instagram.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 4096)
+    private String content;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Post post = (Post) o;
+        return id != null && Objects.equals(id, post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
